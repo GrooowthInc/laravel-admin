bf626f5cb49ad11b09163461d012d83ee8a3696e
a.matsuse <a.matsuse@grooowth.co.jp>
2017-07-26 12:02:48 +0900

    update 保活環境のものをもってきた

--- nursery_search/webpack.mix.js
+++ nursery_search/webpack.mix.js
@@ -1,5 +1,7 @@
-let mix = require('laravel-mix');
-
+const {
+    mix
+} = require('laravel-mix')
+const path = require('path')
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,6 +12,44 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+// Full API
+// mix.js(src, output);
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.less(src, output);
+// mix.combine(files, destination);
+// mix.copy(from, to);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
+
+mix.setResourceRoot('/hokatsu/');
+
+mix.webpackConfig({
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'resources/assets/semantic/dist'),
+            path.resolve(__dirname, 'node_modules')
+        ]
+    }
+})
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .extract(['vue'])
+    .sass('resources/assets/sass/app.scss', 'public/css')
+
+mix.copy('resources/assets/semantic/dist/themes/', 'public/themes/', false)
+mix.copy('node_modules/font-awesome/fonts', 'public/fonts/fontawesome', false)
+mix.copy('resources/assets/images', 'public/images', false)
+if (mix.config.inProduction) {
+    // mix.version();
+}
+
+if (mix.config.inDevelopment) {
+    mix.sourceMaps()
+}
