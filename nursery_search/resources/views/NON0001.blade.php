33e2e69f720b35688be17092ca73df133ea38ee7
a.matsuse <a.matsuse@grooowth.co.jp>
2017-08-02 18:00:32 +0900

    update ＃35039 見出し修正、TOP実装、広告カルーセルパーシャル化

--- nursery_search/resources/views/NON0001.blade.php
+++ nursery_search/resources/views/NON0001.blade.php
@@ -16,7 +16,78 @@
     ニュースのコピーですLorem ipsum dolor sit amet, consectetur adipisicing elit.
   </div>
   <section class="bns-section">
-    <h2 class="bns-title-level2">エリアを選んでください</h2>
+    <h2 class="bns-title-area"><span class="bns-title-area__text">エリアを選んでください</span></h2>
+    <div class="bns-block-active ui grid">
+      <div class="doubling four column row">
+        <div class="column">
+          <button class="bns-btn--primary">東京都</button>
+        </div>
+        <div class="column">
+          <button class="bns-btn--primary">神奈川県</button>
+        </div>
+        <div class="column">
+          <button class="bns-btn--primary">千葉県</button>
+        </div>
+        <div class="column">
+          <button class="bns-btn--primary">埼玉県</button>
+        </div>
+      </div>
+    </div><!-- /.ui grid -->
+    <div class="bns-block-active ui grid centered">
+      <div class="fifteen wide column">
+        <button class="bns-btn--primary">現在地からさがす</button>
+      </div>
+    </div><!-- /.ui grid -->
+
+    <div class="bns-layout">
+      <button class="bns-btn--secondary">保育園選びのポイント</button>
+    </div>
+
+    <h2 class="bns-title-level2"><i class="bns-title-icon"><img src="https://dummyimage.com/40x30/000/808080.png&text=dummy" alt="" /></i>最近見た保育園</h2>
+    <div class="bns-layout--negative">
+      ほげ
+    </div>
+    
+    <!-- ▼広告カルーセル▼ -->
+    @include('_partial.ad-carousel')
+    <!-- ▲広告カルーセル▲ -->
+
+    <h2 class="bns-title-level2"><i class="bns-title-icon"><img src="https://dummyimage.com/40x30/000/808080.png&text=dummy" alt="" /></i>おすすめ記事</h2>
+    <div class="bns-layout--negative">
+      <div class="bns-block-recommend">
+        <a href="#" class="bns-block-recommend__item">
+          <div class="bns-block-recommend__image">
+            <img src="https://dummyimage.com/100x100/000/808080.png&text=dummy" alt="" />
+          </div>
+          <div class="bns-block-recommend__body">
+            <p class="bns-block-recommend__date">2017.08.01</p>
+            <p class="bns-block-recommend__title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </a>
+        <a href="#" class="bns-block-recommend__item">
+          <div class="bns-block-recommend__image">
+            <img src="https://dummyimage.com/100x100/000/808080.png&text=dummy" alt="" />
+          </div>
+          <div class="bns-block-recommend__body">
+            <p class="bns-block-recommend__date">2017.12.17</p>
+            <p class="bns-block-recommend__title">年末年始お休みのお知らせ</p>
+          </div>
+        </a>
+        <a href="#" class="bns-block-recommend__item">
+          <div class="bns-block-recommend__image">
+            <img src="https://dummyimage.com/100x100/000/808080.png&text=dummy" alt="" />
+          </div>
+          <div class="bns-block-recommend__body">
+            <p class="bns-block-recommend__date">2018.01.01</p>
+            <p class="bns-block-recommend__title">明けましておめでとうございます。今年も宜しくお願いいたします。</p>
+          </div>
+        </a>
+      </div>
+    </div><!-- /.bns-layout--negative -->
+    <div class="bns-layout">
+      <button class="bns-btn--secondary">記事一覧へ</button>
+    </div>
+
   </section>
 </article>
 @stop
---
+++ nursery_search/resources/views/_partial/ad-carousel.blade.php
@@ -0,0 +1,37 @@
+<div class="bns-block-ad">
+  <div class="bns-block-ad__heading">園生活におすすめ</div>
+  <ul class="bns-block-ad-list" data-bns-slick="target-recommend">
+    <li class="bns-block-ad-list__item" data-bns-height="carousel">
+      <a href="#" class="bns-block-ad-list__body">
+        <div class="bns-block-ad-list__image">
+          <img src="https://dummyimage.com/135x165/000/808080.png&text=dummy" alt="" />
+        </div>
+        <p class="bns-block-ad-list__text">おすすめアイテムその1</p>
+      </a>
+    </li>
+    <li class="bns-block-ad-list__item" data-bns-height="carousel">
+      <a href="#" class="bns-block-ad-list__body">
+        <div class="bns-block-ad-list__image">
+          <img src="https://dummyimage.com/135x165/000/808080.png&text=dummy" alt="" />
+        </div>
+        <p class="bns-block-ad-list__text">おすすめアイテムその2</p>
+      </a>
+    </li>
+    <li class="bns-block-ad-list__item" data-bns-height="carousel">
+      <a href="#" class="bns-block-ad-list__body">
+        <div class="bns-block-ad-list__image">
+          <img src="https://dummyimage.com/135x165/000/808080.png&text=dummy" alt="" />
+        </div>
+        <p class="bns-block-ad-list__text">テキスト長いよテキスト長いよテキスト長いよテキスト長いよ</p>
+      </a>
+    </li>
+    <li class="bns-block-ad-list__item" data-bns-height="carousel">
+      <a href="#" class="bns-block-ad-list__body">
+        <div class="bns-block-ad-list__image">
+          <img src="https://dummyimage.com/135x165/000/808080.png&text=dummy" alt="" />
+        </div>
+        <p class="bns-block-ad-list__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </a>
+    </li>
+  </ul><!-- /.bns-carousel -->
+</div><!-- /.bns-block-ad -->
