b5f77ef161304d50aec801a9c0b66f223d018f7b
a.matsuse <a.matsuse@grooowth.co.jp>
2017-08-07 16:54:07 +0900

    update ＃35039 フッターFB吸収

--- nursery_search/resources/views/_partial/footer.blade.php
+++ nursery_search/resources/views/_partial/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="bns-footer">
+  <h4 class="bns-title-level4">ご利用について</h4>
+  <p class="bns-text">保育園情報は、ベネッセコーポレーション「保活ナビ」が独自に実施したアンケートの内容を基に掲載しております。<br>掲載内容については、調査時点の内容となりますので、必ず各自治体が公開している最新情報等をご確認ください。</p>
   <a href="#" class="bns-text-link">会社案内</a>
-  <a href="#" class="bns-text-link">個人情報保護への取り組みについて</a>
   <p class="bns-footer-copyright">&copy; Benesse&nbsp;Corporation&nbsp;2000&nbsp;-&nbsp;2014.</p>
 </footer><!-- .bns-footer -->
