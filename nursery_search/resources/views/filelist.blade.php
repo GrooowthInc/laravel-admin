923fef5f0425ce424161a9e3f3e169820e5dabc8
a.matsuse <a.matsuse@grooowth.co.jp>
2017-08-09 11:45:34 +0900

    update ＃35039 filelist更新（途中）

--- nursery_search/resources/views/filelist.blade.php
+++ nursery_search/resources/views/filelist.blade.php
@@ -50,135 +50,302 @@ table tr:nth-child(even) td {
 </head>
 <body>
 
-<h1>保活 画面一覧</h1>
+  <h1>保活 画面一覧</h1>
 
-<table>
-  <thead>
-    <tr>
-      <th style="width:6.0em;">画面ID</th>
-      <th style="width:30.0em;">画面名</th>
-      <th>URL</th>
-      <!-- <th style="width:6.0em;">フェーズ</th> -->
-      <th style="width:10.0em;">パターン</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>modules</td>
-      <td>モジュールリスト</td>
-      <td><a href="{{ url('/modules') }}" target="_blank">modules</a></td>
-      <!-- <td>4中</td> -->
-      <td>モジュール</td>
-    </tr>
-    <tr>
-      <td>NON0001</td>
-      <td>トップページ</td>
-      <td><a href="{{ url('/') }}" target="_blank">NON0001</a></td>
-      <!-- <td>4中</td> -->
-      <td>トップページ</td>
-    </tr>
-    <tr>
-      <td>NON0002</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0002') }}" target="_blank">NON0002</a></td>
-      <!-- <td>4中</td> -->
-      <td>市区町村選択</td>
-    </tr>
-    <tr>
-      <td>NON0003</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0003') }}" target="_blank">NON0003</a></td>
-      <!-- <td>4中</td> -->
-      <td>沿線選択</td>
-    </tr>
-    <tr>
-      <td>NON0004</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0004') }}" target="_blank">NON0004</a></td>
-      <!-- <td>4中</td> -->
-      <td>駅名選択</td>
-    </tr>
-    <tr>
-      <td>NON0005</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0005') }}" target="_blank">NON0005</a></td>
-      <!-- <td>4中</td> -->
-      <td>地図選択</td>
-    </tr>
-    <tr>
-      <td>NON0006</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0006') }}" target="_blank">NON0006</a></td>
-      <!-- <td>4中</td> -->
-      <td>保育園検索結果一覧</td>
-    </tr>
-    <tr>
-      <td>NON0007</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0007') }}" target="_blank">NON0007</a></td>
-      <!-- <td>4中</td> -->
-      <td>施設詳細</td>
-    </tr>
-    <tr>
-      <td>NON0008</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0008') }}" target="_blank">NON0008</a></td>
-      <!-- <td>4中</td> -->
-      <td>ご利用規約</td>
-    </tr>
-    <tr>
-      <td>NON0009</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0009') }}" target="_blank">NON0009</a></td>
-      <!-- <td>4中</td> -->
-      <td>ご利用規約</td>
-    </tr>
-    <tr>
-      <td>NON0010</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0010') }}" target="_blank">NON0010</a></td>
-      <!-- <td>4中</td> -->
-      <td>ご利用規約</td>
-    </tr>
-    <tr>
-      <td>NON0011</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0011') }}" target="_blank">NON0011</a></td>
-      <!-- <td>4中</td> -->
-      <td>ご利用規約</td>
-    </tr>
-    <tr>
-      <td>NON0012</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0012') }}" target="_blank">NON0012</a></td>
-      <!-- <td>4中</td> -->
-      <td>ご利用規約</td>
-    </tr>
-    <tr>
-      <td>NON0013</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0013') }}" target="_blank">NON0013</a></td>
-      <!-- <td>4中</td> -->
-      <td>ご利用規約</td>
-    </tr>
-    <tr>
-      <td>NON0014</td>
-      <td>保育園検索</td>
-      <td><a href="{{ url('/NON0014') }}" target="_blank">NON0014</a></td>
-      <!-- <td>4中</td> -->
-      <td>ご利用規約</td>
-    </tr>
-    <tr>
-      <td>NON0015</td>
-      <td>エラーページ</td>
-      <td><a href="{{ url('/NON0015') }}" target="_blank">NON0015</a></td>
-      <!-- <td>4中</td> -->
-      <td>エラーページ</td>
-    </tr>
-  </tbody>
-</table>
+  <h2>画面一覧</h2>
+  <table>
+    <thead>
+      <tr>
+        <th style="width:6.0em;">画面ID</th>
+        <th style="width:30.0em;">画面名</th>
+        <th>URL</th>
+        <th style="width:6.0em;">GRI最終更新日</th>
+        <th style="width:6.0em;">デザインFIX状況</th>
+        <th style="width:6.0em;">CQ最終更新日</th>
+        <th style="width:10.0em;">パターン</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>modules</td>
+        <td>モジュールリスト</td>
+        <td><a href="{{ url('/modules') }}" target="_blank">modules</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>モジュール</td>
+      </tr>
+      <tr>
+        <td>NON0001</td>
+        <td>トップページ</td>
+        <td><a href="{{ url('/') }}" target="_blank">NON0001</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>トップページ</td>
+      </tr>
+      <tr>
+        <td>NON0002</td>
+        <td>保育園検索</td>
+        <td><a href="{{ url('/NON0002') }}" target="_blank">NON0002</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>市区町村選択</td>
+      </tr>
+      <tr>
+        <td>NON0003</td>
+        <td>保育園検索</td>
+        <td><a href="{{ url('/NON0003') }}" target="_blank">NON0003</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>沿線選択</td>
+      </tr>
+      <tr>
+        <td>NON0004</td>
+        <td>保育園検索</td>
+        <td><a href="{{ url('/NON0004') }}" target="_blank">NON0004</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>駅名選択</td>
+      </tr>
+      <tr>
+        <td>NON0005</td>
+        <td>保育園検索</td>
+        <td><a href="{{ url('/NON0005') }}" target="_blank">NON0005</a></td>
+        <td>8/9</td>
+        <td>0%</td>
+        <td>-</td>
+        <td>地図選択</td>
+      </tr>
+      <tr>
+        <td>NON0006</td>
+        <td>保育園検索</td>
+        <td><a href="{{ url('/NON0006') }}" target="_blank">NON0006</a></td>
+        <td>8/9</td>
+        <td>80%</td>
+        <td>-</td>
+        <td>保育園検索結果一覧</td>
+      </tr>
+      <tr>
+        <td>NON0007</td>
+        <td>保育園検索</td>
+        <td><a href="{{ url('/NON0007') }}" target="_blank">NON0007</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>施設詳細</td>
+      </tr>
+      <tr>
+        <td>NON0008</td>
+        <td>ご利用規約</td>
+        <td><a href="{{ url('/NON0008') }}" target="_blank">NON0008</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>ご利用規約</td>
+      </tr>
+      <tr>
+        <td>NON0009</td>
+        <td>保育園検索</td>
+        <td><a href="{{ url('/NON0009') }}" target="_blank">NON0009</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>保育園選びのポイント</td>
+      </tr>
+      <tr>
+        <td>NON0010</td>
+        <td>サービス概要</td>
+        <td><a href="{{ url('/NON0010') }}" target="_blank">NON0010</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>サービス概要</td>
+      </tr>
+      <tr>
+        <td>NON0011</td>
+        <td>あとで見る一覧</td>
+        <td><a href="{{ url('/NON0011') }}" target="_blank">NON0011</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>あとで見る一覧</td>
+      </tr>
+      <tr>
+        <td>NON0012</td>
+        <td>おすすめ記事一覧</td>
+        <td><a href="{{ url('/NON0012') }}" target="_blank">NON0012</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>おすすめ記事一覧</td>
+      </tr>
+      <tr>
+        <td>NON0013</td>
+        <td>FAQ</td>
+        <td><a href="{{ url('/NON0013') }}" target="_blank">NON0013</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>FAQ</td>
+      </tr>
+      <tr>
+        <td>NON0014</td>
+        <td>サイトマップ</td>
+        <td><a href="{{ url('/NON0014') }}" target="_blank">NON0014</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>サイトマップ</td>
+      </tr>
+      <tr>
+        <td>NON0015</td>
+        <td>エラーページ</td>
+        <td><a href="{{ url('/NON0015') }}" target="_blank">NON0015</a></td>
+        <td>8/9</td>
+        <td>90%</td>
+        <td>-</td>
+        <td>エラーページ</td>
+      </tr>
+    </tbody>
+  </table>
 
-<script src="assets/js/app.js" defer></script>
+  <h2>レイアウト一覧</h2>
+  <table>
+    <thead>
+      <tr>
+        <th style="width:6.0em;">画面ID</th>
+        <th style="width:30.0em;">画面名</th>
+        <th>URL</th>
+        <th style="width:6.0em;">GRI最終更新日</th>
+        <th style="width:6.0em;">デザインFIX状況</th>
+        <th style="width:6.0em;">CQ最終更新日</th>
+        <th style="width:10.0em;">パターン</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>default</td>
+        <td>レイアウト デフォルト</td>
+        <td><a href="{{ url('/layouts/default') }}" target="_blank">default</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>デフォルト</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <h2>パーシャル一覧</h2>
+  <table>
+    <thead>
+      <tr>
+        <th style="width:6.0em;">画面ID</th>
+        <th style="width:30.0em;">画面名</th>
+        <th>URL</th>
+        <th style="width:6.0em;">GRI最終更新日</th>
+        <th style="width:6.0em;">デザインFIX状況</th>
+        <th style="width:6.0em;">CQ最終更新日</th>
+        <th style="width:10.0em;">パターン</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>ad-carousel</td>
+        <td>カルーセル</td>
+        <td><a href="{{ url('/partial/ad-carousel') }}" target="_blank">ad-carousel</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>広告カルーセル</td>
+      </tr>
+      <tr>
+        <td>banner-area</td>
+        <td>バナーエリア</td>
+        <td><a href="{{ url('/partial/banner-area') }}" target="_blank">banner-area</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>広告バナー</td>
+      </tr>
+      <tr>
+        <td>breadcrumbs</td>
+        <td>ぱんくず</td>
+        <td><a href="{{ url('/partial/breadcrumbs') }}" target="_blank">breadcrumbs</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>ぱんくず</td>
+      </tr>
+      <tr>
+        <td>facility-carousel</td>
+        <td>カルーセル</td>
+        <td><a href="{{ url('/partial/facility-carousel') }}" target="_blank">facility-carousel</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>施設詳細カルーセル</td>
+      </tr>
+      <tr>
+        <td>footer</td>
+        <td>フッター</td>
+        <td><a href="{{ url('/partial/footer') }}" target="_blank">footer</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>フッター</td>
+      </tr>
+      <tr>
+        <td>header</td>
+        <td>ヘッダー</td>
+        <td><a href="{{ url('/partial/header') }}" target="_blank">header</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>ヘッダー</td>
+      </tr>
+      <tr>
+        <td>near-nursery</td>
+        <td>近隣の保育園</td>
+        <td><a href="{{ url('/partial/near-nursery') }}" target="_blank">near-nursery</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>近隣の保育園</td>
+      </tr>
+      <tr>
+        <td>recently-see-nursery</td>
+        <td>最近見た保育園</td>
+        <td><a href="{{ url('/partial/recently-see-nursery') }}" target="_blank">recently-see-nursery</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>最近見た保育園</td>
+      </tr>
+      <tr>
+        <td>recommend-news</td>
+        <td>おすすめ記事</td>
+        <td><a href="{{ url('/partial/recommend-news') }}" target="_blank">recommend-news</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>おすすめ記事</td>
+      </tr>
+      <tr>
+        <td>see-later</td>
+        <td>あとで見る</td>
+        <td><a href="{{ url('/partial/see-later') }}" target="_blank">see-later</a></td>
+        <td>8/9</td>
+        <td>100%</td>
+        <td>-</td>
+        <td>あとで見る</td>
+      </tr>
+    </tbody>
+  </table>
+
 </body>
 </html>
 
